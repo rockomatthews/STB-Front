fb30de1675162b2c3e7af8f833f44285921df86e
rockomatthews <rob@robmatthe.ws>
2024-08-21 13:25:20 -0500

    app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme/theme';  // Import the theme you've created
 import Home from './pages/Home';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
@@ -9,12 +12,15 @@ import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* This component helps in applying consistent baseline styles */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
